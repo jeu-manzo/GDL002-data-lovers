680d735c97b73f6aeee3c689983de9d09929d145
Belen-Alejandra <alejandra_ponce_89@hotmail.com>
2019-02-21 07:39:58 -0800

    Agregue la funcion para indicadores de Peru y Mexico

--- src/data.js
+++ src/data.js
@@ -12,9 +12,47 @@ const indicatorCountryPer = window.WORLDBANK.PER.indicators.map
   indicatorName, indicatorCode
 }) => [indicatorName, indicatorCode]);
 
-//Filtrar datos{}
-const nombre = indicatorCountryPer[0];
-const result = nombre.filter(codigo => codigo.value === "SG.VAW.REFU.ZS");
+//Filtrar datos de indicadores culturales para Peru{}
 
-//window.WORLDBANK.PER.indicators[0].indicatorCode;
-console.log(result);
+const prueba = window.WORLDBANK.PER.indicators;
+//console.log(prueba);
+//Muestra los indicadores seleccionados con todos los datos
+const pruebaPeru = prueba.filter(indicator => (indicator.indicatorCode == "SG.VAW.REFU.ZS" || indicator.indicatorCode =="SG.VAW.REAS.ZS"
+|| indicator.indicatorCode =="SG.VAW.NEGL.ZS"
+|| indicator.indicatorCode =="SG.VAW.GOES.ZS"
+|| indicator.indicatorCode =="SG.VAW.BURN.ZS"
+|| indicator.indicatorCode =="SG.VAW.ARGU.ZS"
+|| indicator.indicatorCode =="SP.M18.2024.FE.ZS"
+));
+console.log(pruebaPeru);
+//Muestra solo los nombres de los indicadores seleccionados
+const indCulPer = pruebaPeru.map(indicator => indicator.indicatorName);
+console.log(indCulPer);
+
+//Muestra en una lista los nombres de los indicadores seleccionados
+indiPer = "<ul>";
+for (i = 0; i < indCulPer.length; i++) {
+  indiPer += "<li>" + indCulPer[i] + "</li>";
+}
+indiPer += "</ul>";
+document.getElementById("demo").innerHTML = indiPer;
+//console.log(text);
+
+
+
+const pruebaMex = window.WORLDBANK.MEX.indicators.filter(indicator => (indicator.indicatorCode == "SG.VAW.REFU.ZS" || indicator.indicatorCode =="SG.VAW.REAS.ZS"
+|| indicator.indicatorCode =="SG.VAW.NEGL.ZS"
+|| indicator.indicatorCode =="SG.VAW.GOES.ZS"
+|| indicator.indicatorCode =="SG.VAW.BURN.ZS"
+|| indicator.indicatorCode =="SG.VAW.ARGU.ZS"
+|| indicator.indicatorCode =="SP.M18.2024.FE.ZS"
+));
+const indCulMex = pruebaMex.map(indicator => indicator.indicatorName);
+//console.log(indCulPer);
+//console.log(pruebaMex);;
+indiMex = "<ul>";
+for (i = 0; i < indCulMex.length; i++) {
+  indiMex += "<li>" + indCulMex[i] + "</li>";
+}
+indiMex += "</ul>";
+document.getElementById("demo1").innerHTML = indiPer;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 let sectionTwo = document.getElementById("entendiendo-violencia");
 let sectionFlags = document.getElementById("section-flags");
 let sectionIndicator = document.getElementById("section-indicator");
+let sectionPeru = document.getElementById("country-per");
+let sectionMex = document.getElementById("country-mex");
 
 function display (elements, display = 'inline-block') {
   for (let i = 0; i < elements.length; i++) {
@@ -9,22 +11,35 @@ function display (elements, display = 'inline-block') {
   }
 }
 
-display([sectionTwo, sectionFlags, sectionIndicator], "none");
+display([sectionTwo, sectionFlags, sectionIndicator,sectionPeru,sectionMex], "none");
 
 
 document.getElementById("btn-data").addEventListener("click", buttonData);
 function buttonData() {
+  display([sectionPeru,sectionFlags,sectionIndicator,sectionMex], "none");
   display([sectionTwo]);
 }
 
 document.getElementById("btn-country").addEventListener("click", buttonCountry);
 function buttonCountry() {
-  display([sectionTwo], "none");
+  display([sectionTwo,sectionPeru,sectionMex], "none");
   display([sectionFlags]);
+}
+
+document.getElementById("peru").addEventListener("click", buttonCountryPeru);
+function buttonCountryPeru() {
+  display([sectionTwo,sectionFlags], "none");
+  display([sectionPeru]);
+}
+
+document.getElementById("mexico").addEventListener("click", buttonCountryMex);
+function buttonCountryMex() {
+  display([sectionTwo,sectionFlags,sectionPeru], "none");
+  display([sectionMex]);
 }
 
 document.getElementById("btn-indicator").addEventListener("click", buttonIndicator);
 function buttonIndicator() {
-  display([sectionTwo, sectionFlags], "none");
+  display([sectionTwo, sectionFlags,sectionPeru], "none");
   display([sectionIndicator]);
 }
