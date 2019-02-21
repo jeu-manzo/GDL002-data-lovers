7a6b21506311f1848b5c0f95c34347ee3ddc6973
Jeu <jeunettemanzo@gmail.com>
2019-02-20 20:40:34 -0600

    muestra en consola los indicadores de percepcion cultural de peru

--- src/data.js
+++ src/data.js
@@ -1,20 +1,34 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-// const example = () => {
-//   return 'example';
-// };
-//
-// window.example = example;
 
+//Obtener en un array los valores de indicatorName y indicatorCode
 const indicatorCountryPer = window.WORLDBANK.PER.indicators.map
 (({
   indicatorName, indicatorCode
 }) => [indicatorName, indicatorCode]);
 
-//Filtrar datos{}
-const nombre = indicatorCountryPer[0];
-const result = nombre.filter(codigo => codigo.value === "SG.VAW.REFU.ZS");
+const indicatorsPerceptionCult = ["SG.VAW.REFU.ZS",
+"SG.VAW.REAS.ZS",
+"SG.VAW.NEGL.ZS",
+"SG.VAW.GOES.ZS",
+"SG.VAW.BURN.ZS",
+"SG.VAW.ARGU.ZS",
+"SP.M18.2024.FE.ZS"
+]
 
-//window.WORLDBANK.PER.indicators[0].indicatorCode;
-console.log(result);
+//Imprimir solo indicatorCode
+const values = []
+indicatorCountryPer.forEach(function(element) {
+  const name = element[0]
+  const code = element[1]
+
+  const isIn = indicatorsPerceptionCult.findIndex(function (indicatorCode) {
+    return indicatorCode === code
+  })
+
+  if (isIn !== -1) {
+    // values.push(element[1]);
+    console.log(name)
+  }
+})
