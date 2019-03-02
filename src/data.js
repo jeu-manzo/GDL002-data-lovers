27ce507a50500afa3ce36fc28846e644b13f029e
Jeu <jeunettemanzo@gmail.com>
2019-03-01 18:55:46 -0600

    pruebas test

--- src/data.js
+++ src/data.js
@@ -1,95 +1,56 @@
-const indicatorsPerceptionCult = [
-  { indicatorCode: "SG.VAW.REFU.ZS",
-  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando ella se niega a tener relaciones sexuales (%)" },
-  { indicatorCode: "SG.VAW.REAS.ZS",
-  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa (cualquiera de las cinco razones) (%)" },
-  { indicatorCode: "SG.VAW.NEGL.ZS",
-  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando descuida a los hijos (%)" },
-  { indicatorCode: "SG.VAW.GOES.ZS",
-  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando sale sin avisarle (%)" },
-  { indicatorCode: "SG.VAW.BURN.ZS",
-  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando a ella se le quema la comida (%)" },
-  { indicatorCode: "SG.VAW.ARGU.ZS",
-  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando ella discute con él (%)" },
-  { indicatorCode: "SP.M18.2024.FE.ZS",
-  indicatorName: "Mujeres que se casaron por primera vez antes de los 18 (% de mujeres 10-24)" },
-];
-
-const indicatorsLiteracy = [
-  { indicatorCode: 'SE.TER.CUAT.MS.FE.ZS',
-   indicatorName: 'Nivel de instrucción, al menos nivel de maestría o equivalente, población de más de 25 años, mujeres (%) (acumulativo)' },
-  { indicatorCode: 'SE.TER.CUAT.DO.FE.ZS',
-  indicatorName: 'Nivel de instrucción, al menos nivel de doctorado o equivalente, población de más de 25 años, mujeres (%) (acumulativo)' },
-  { indicatorCode: 'SE.SEC.CUAT.PO.FE.ZS',
-  indicatorName: 'Nivel de instrucción, al menos escuela de nivel posterior a la secundaria completa, población de más de 25 años, mujeres (%) (acumulativo)' },
-  { indicatorCode: 'SE.PRM.CUAT.FE.ZS',
-  indicatorName: 'Nivel de instrucción, al menos escuela primaria completa, población de más de 25 años, mujeres (%) (acumulativo)' },
-  { indicatorCode: 'SE.ADT.LITR.FE.ZS',
-  indicatorName: 'Tasa de alfabetización, mujeres adultas (% de mujeres de 15 años o más)' },
-]
-
-const indicatorUnemployment = [
-  { indicatorCode: "SL.UEM.INTM.FE.ZS",
-    indicatorName: "Personas desempleadas con educación intermedia, mujeres"},
-  { indicatorCode:  "SL.UEM.BASC.FE.ZS",
-    indicatorName: "Personas desempleadas con educación básica, mujeres"},
-  { indicatorCode: "SL.UEM.ADVN.FE.ZS",
-    indicatorName: "Personas desempleadas con educación avanzada, mujeres"},
-  { indicatorCode: "SL.UEM.NEET.FE.ZS",
-    indicatorName: "Proporción de jóvenes sin educación, empleo ni capacitación, mujeres (% de la población de mujeres jóvenes)"},
-];
-
-function filterCountry(country) {
-  const dataCountry = WORLDBANK[country]
+window.dataLovers = {
+filterCountry: function filterCountry(country) {
+  const dataCountry = WORLDBANK[country];
 
   const indicatorsCountry = dataCountry.indicators.filter(function (indicator) {
-    const indicatorCode = indicator.indicatorCode
+    const indicatorCode = indicator.indicatorCode;
     const indicatorCodePerception = indicatorsPerceptionCult.map(ind => ind.indicatorCode);
     const isIn = indicatorCodePerception.findIndex(function (val) {
-      return val === indicatorCode
-    })
+      return val === indicatorCode;
+    });
 
-    return isIn !== -1
-  })
-  return indicatorsCountry
-}
+    return isIn !== -1;
+  });
+  return indicatorsCountry;
+
+},
 
 
-function showIndicatorNames(indicators, parentElement) {
+showIndicatorNamesCountry: function showIndicatorNamesCountry(indicators, parentElement) {
   indicators.forEach(function(element) {
     const indicatorValue = element;
     const newP = document.createElement("p");
     const link = document.createElement("a");
-    const newPID = newP.setAttribute("class", "one-indicator")
-    const newPSetCode = link.setAttribute("data-code", indicatorValue.indicatorCode)
-    const newPSetCountry = link.setAttribute("data-country", indicatorValue.countryCode)
-    link.href = '#'
+    newP.setAttribute("class", "one-indicator");
+    link.setAttribute("data-code", indicatorValue.indicatorCode);
+    link.setAttribute("data-country", indicatorValue.countryCode);
+    link.href = '#';
     const content = document.createTextNode(indicatorValue.indicatorName);
     newP.appendChild(link);
     link.appendChild(content);
     parentElement.parentNode.insertBefore(newP, parentElement.nextSibling);
   });
 
-}
+},
 
 
-function filterIndicatorYear(code, country) {
-  const countryInd = filterCountry(country)
-  const dataYear = countryInd.filter(indicator => indicator.indicatorCode == code)
+filterIndicatorYear: function filterIndicatorYear(code, country) {
+  const countryInd = window.dataLovers.filterCountry(country);
+  const dataYear = countryInd.filter(indicator => indicator.indicatorCode == code);
 
-  return dataYear[0].data
+  return dataYear[0].data;
 
-}
+},
 
 
 //Mostrar en pantalla Indicadores por categoria
-function showAllIndicators(category, parentElement) {
+showAllIndicators: function showAllIndicators(category, parentElement) {
   category.forEach(function(element) {
     const indicatorValue = element;
     const newP = document.createElement("p");
     const link = document.createElement("a");
-    const newPClass = link.setAttribute("class", "each-indicator");
-    newPSetCode = link.setAttribute("data-code", indicatorValue.indicatorCode);
+    link.setAttribute("class", "each-indicator");
+    link.setAttribute("data-code", indicatorValue.indicatorCode);
     // const newPSetCountry = link.setAttribute("data-country", indicatorValue.countryCode)
     link.href = '#';
     const content = document.createTextNode(indicatorValue.indicatorName);
@@ -99,69 +60,48 @@ function showAllIndicators(category, parentElement) {
 
   });
 
-}
+},
+
+};
+
+// const arrayExample = [5.0, 8.4, 3, 2 , 6];
+//
+// function orderAscendant(array) {
+//   array.sort(function(a, b) {
+//     return a - b;
+//   });
+//   return array;
+// }
+//
+// function orderDescendant(array) {
+//   array.sort(function(a, b) {
+//     return b - a;
+//   });
+//   return array;
+// }
+//
+// console.log(orderAscendant(arrayExample));
 
 //Trabajar despues
 // //Filtrar información de cada indicadores por categoría de todos los países
 // function filterIndicatorCategory(categories, code) {
-//   const dataCountry = WORLDBANK;
-//   const arrayData = Object.values(dataCountry);
-//   arrayData.forEach(function(element) {
-//     const indicatorCountry = element.indicators
-//     const filterIndicator = indicatorCountry.filter(function (indicator) {
-//       const indicatorCode = indicator.indicatorCode
-//       //console.log(indicatorCode);
-//       const indicatorsCode = categories.map(ind => ind.indicatorCode);
-//       const isIn = indicatorsCode.findIndex(function (val) {
-//         return val === indicatorCode
-//       })
-//
-//       return isIn !== -1
-//     })
-//
-//     filterIndicator.forEach(function(element) {
-//       console.log(element);
-//     });
-//   });
-// }
-
-let indicatorGrales = (code) => {
-      indiPer = WORLDBANK.PER.indicators.filter(indicator => indicator.indicatorCode == code);
-      indiMex = WORLDBANK.MEX.indicators.filter(indicator => indicator.indicatorCode == code);
-       indiBra = WORLDBANK.BRA.indicators.filter(indicator => indicator.indicatorCode == code);
-       indiChl = WORLDBANK.CHL.indicators.filter(indicator => indicator.indicatorCode == code);
-       indiGral = [indiBra,indiChl,indiMex,indiPer];
-       return indiGral;
-    };
-
-//retorna nombre paises
-indicatorTableCountry = (code) => {
-  const pruebas21=[];
-  indicatorGrales(code).forEach(function(e) {
-    pruebas21.push(e[0]);
-  })
-   fullNames = pruebas21.map(inventor => inventor.countryName);
-   return fullNames;
-  };
-
-//retorna data
-indicatorTableData = (code) => {
-  const pruebas12=[];
-  indicatorGrales(code).forEach(function(e) {
-    pruebas12.push(e[0]);
-  })
-   fullData = pruebas12.map(inventor => inventor.data);
-   return fullData;
-  };
-
-
-
-//   function filterYearAllContries(year) {
-//     const years = indicatorTableData('SG.VAW.REFU.ZS');
-//     const arr = Object.values(years)
-//     console.log(arr.filter(word => word.0.3)));
-//
-//   }
-// //arr.filter(word => word.2006)
-// // mapyears = element.map(x => x.year);
-//   console.log(filterYearAllContries(2006));
+  //   const dataCountry = WORLDBANK;
+  //   const arrayData = Object.values(dataCountry);
+  //   arrayData.forEach(function(element) {
+    //     const indicatorCountry = element.indicators
+    //     const filterIndicator = indicatorCountry.filter(function (indicator) {
+      //       const indicatorCode = indicator.indicatorCode
+      //       //console.log(indicatorCode);
+      //       const indicatorsCode = categories.map(ind => ind.indicatorCode);
+      //       const isIn = indicatorsCode.findIndex(function (val) {
+        //         return val === indicatorCode
+        //       })
+        //
+        //       return isIn !== -1
+        //     })
+        //
+        //     filterIndicator.forEach(function(element) {
+          //       console.log(element);
+          //     });
+          //   });
+          // }
--- src/main.js
+++ src/main.js
@@ -8,6 +8,47 @@ const sectionCategories = document.getElementById('categories');
 const sectionIndicatorCountries = document.getElementById('indicator-countries');
 
 
+const indicatorsPerceptionCult = [
+  { indicatorCode: "SG.VAW.REFU.ZS",
+  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando ella se niega a tener relaciones sexuales (%)" },
+  { indicatorCode: "SG.VAW.REAS.ZS",
+  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa (cualquiera de las cinco razones) (%)" },
+  { indicatorCode: "SG.VAW.NEGL.ZS",
+  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando descuida a los hijos (%)" },
+  { indicatorCode: "SG.VAW.GOES.ZS",
+  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando sale sin avisarle (%)" },
+  { indicatorCode: "SG.VAW.BURN.ZS",
+  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando a ella se le quema la comida (%)" },
+  { indicatorCode: "SG.VAW.ARGU.ZS",
+  indicatorName: "Mujeres que creen que está justificado que un marido golpee a su esposa cuando ella discute con él (%)" },
+  { indicatorCode: "SP.M18.2024.FE.ZS",
+  indicatorName: "Mujeres que se casaron por primera vez antes de los 18 (% de mujeres 10-24)" },
+];
+
+const indicatorsLiteracy = [
+  { indicatorCode: 'SE.TER.CUAT.MS.FE.ZS',
+   indicatorName: 'Nivel de instrucción, al menos nivel de maestría o equivalente, población de más de 25 años, mujeres (%) (acumulativo)' },
+  { indicatorCode: 'SE.TER.CUAT.DO.FE.ZS',
+  indicatorName: 'Nivel de instrucción, al menos nivel de doctorado o equivalente, población de más de 25 años, mujeres (%) (acumulativo)' },
+  { indicatorCode: 'SE.SEC.CUAT.PO.FE.ZS',
+  indicatorName: 'Nivel de instrucción, al menos escuela de nivel posterior a la secundaria completa, población de más de 25 años, mujeres (%) (acumulativo)' },
+  { indicatorCode: 'SE.PRM.CUAT.FE.ZS',
+  indicatorName: 'Nivel de instrucción, al menos escuela primaria completa, población de más de 25 años, mujeres (%) (acumulativo)' },
+  { indicatorCode: 'SE.ADT.LITR.FE.ZS',
+  indicatorName: 'Tasa de alfabetización, mujeres adultas (% de mujeres de 15 años o más)' },
+];
+
+const indicatorUnemployment = [
+  { indicatorCode: "SL.UEM.INTM.FE.ZS",
+    indicatorName: "Personas desempleadas con educación intermedia, mujeres"},
+  { indicatorCode:  "SL.UEM.BASC.FE.ZS",
+    indicatorName: "Personas desempleadas con educación básica, mujeres"},
+  { indicatorCode: "SL.UEM.ADVN.FE.ZS",
+    indicatorName: "Personas desempleadas con educación avanzada, mujeres"},
+  { indicatorCode: "SL.UEM.NEET.FE.ZS",
+    indicatorName: "Proporción de jóvenes sin educación, empleo ni capacitación, mujeres (% de la población de mujeres jóvenes)"},
+];
+
 function display (elements, display = 'inline-block') {
   for (let i = 0; i < elements.length; i++) {
     elements[i].style.display = display;
@@ -41,10 +82,11 @@ document.getElementById("mexico").addEventListener("click", buttonFlagMexico);
 function buttonFlagMexico() {
   display([sectionTwo, sectionFlags, sectionIndicator,sectionCategories], "none");
   display([sectionCountry]);
-  country = document.getElementById("mexico").dataset.country
-  let result = filterCountry(country)
+  country = document.getElementById("mexico").dataset.country;
+  let result = window.dataLovers.filterCountry(country);
+  console.log(result);
   const countryNameElement = document.getElementById('country-name');
-  showIndicatorNames(result, countryNameElement)
+  window.dataLovers.showIndicatorNamesCountry(result, countryNameElement);
   document.getElementById('title-country').innerHTML = 'Mexico';
   getElementsforEvents('one-indicator', linkIndicator);
 }
@@ -53,10 +95,10 @@ document.getElementById("peru").addEventListener("click", buttonFlagPeru);
 function buttonFlagPeru() {
   display([sectionTwo, sectionFlags, sectionIndicator, sectionCategories], "none");
   display([sectionCountry]);
-  country = document.getElementById("peru").dataset.country
-  let result = filterCountry(country)
+  country = document.getElementById("peru").dataset.country;
+  let result = window.dataLovers.filterCountry(country);
   const countryNameElement = document.getElementById('country-name');
-  showIndicatorNames(result, countryNameElement)
+  window.dataLovers.showIndicatorNamesCountry(result, countryNameElement);
   document.getElementById('title-country').innerHTML = 'Perú';
   getElementsforEvents('one-indicator', linkIndicator);
 }
@@ -65,10 +107,10 @@ document.getElementById("chile").addEventListener("click", buttonFlagChile);
 function buttonFlagChile() {
   display([sectionTwo, sectionFlags, sectionIndicator, sectionCategories], "none");
   display([sectionCountry]);
-  country = document.getElementById("chile").dataset.country
-  let result = filterCountry(country)
+  country = document.getElementById("chile").dataset.country;
+  let result = window.dataLovers.filterCountry(country);
   const countryNameElement = document.getElementById('country-name');
-  showIndicatorNames(result, countryNameElement)
+  window.dataLovers.showIndicatorNamesCountry(result, countryNameElement);
   document.getElementById('title-country').innerHTML = 'Chile';
   getElementsforEvents('one-indicator', linkIndicator);
 }
@@ -77,10 +119,10 @@ document.getElementById("brasil").addEventListener("click", buttonFlagBrasil);
 function buttonFlagBrasil() {
   display([sectionTwo, sectionFlags, sectionIndicator, sectionCategories], "none");
   display([sectionCountry]);
-  country = document.getElementById("brasil").dataset.country
-  let result = filterCountry(country);
+  country = document.getElementById("brasil").dataset.country;
+  let result = window.dataLovers.filterCountry(country);
   const countryNameElement = document.getElementById('country-name');
-  showIndicatorNames(result, countryNameElement);
+  window.dataLovers.showIndicatorNamesCountry(result, countryNameElement);
   document.getElementById('title-country').innerHTML = 'Brasil';
   getElementsforEvents('one-indicator', linkIndicator);
 }
@@ -94,8 +136,8 @@ function linkIndicator() {
     //obtener años por indicador por país
   const codeIndicator = event.srcElement.dataset.code;
   const countryIndicator = event.srcElement.dataset.country;
-  const objData = filterIndicatorYear(codeIndicator, countryIndicator);
-  console.log(objData);
+  const objData = window.dataLovers.filterIndicatorYear(codeIndicator, countryIndicator);
+  //console.log(objData);
   createTableYears(objData);
 }
 
@@ -105,8 +147,8 @@ function createTableYears(years) {
   const mybody = document.getElementById("indicator-table-country");
 
   // creates <table> and <tbody> elements
-  table     = document.createElement("table");
-  tablebody = document.createElement("tbody");
+  const table = document.createElement("table");
+  const tablebody = document.createElement("tbody");
 
   const header = document.createElement("tr");
 
@@ -121,18 +163,20 @@ function createTableYears(years) {
   // creating all cells
   for(var j = 0; j < Object.keys(years).length; j++) {
     var year = Object.keys(years)[j];
-    var percent = Object.values(years)[j];
+    var arrayPercent = Object.values(years);
+    var percent = arrayPercent[j];
     // creates a <tr> element
-      mycurrent_row = document.createElement("tr");
+      const mycurrent_row = document.createElement("tr");
           // creates a <td> element's
-          year_cell = document.createElement("td");
-          percent_cell = document.createElement("td");
+          const year_cell = document.createElement("td");
+          const percent_cell = document.createElement("td");
           // creates a Text Node
+          let currenttext2 = "";
     if (percent == "") { currenttext2 = document.createTextNode("no info.");
       } else {
         currenttext2 = document.createTextNode(percent);
       }
-          currenttext = document.createTextNode(year);
+          let currenttext = document.createTextNode(year);
           // appends the Text Node we created into the cell <td>
           year_cell.appendChild(currenttext);
           percent_cell.appendChild(currenttext2);
@@ -146,8 +190,8 @@ function createTableYears(years) {
   }
 
 
-          titleYears = document.createTextNode("Años");
-          titlePercents = document.createTextNode("Porcentaje %");
+          const titleYears = document.createTextNode("Años");
+          const titlePercents = document.createTextNode("Porcentaje %");
 
           cellYears.appendChild(titleYears);
           cellPercent.appendChild(titlePercents);
@@ -159,14 +203,30 @@ function createTableYears(years) {
   mybody.appendChild(table);
   // sets the border attribute of mytable to 2;
   table.setAttribute("border","2");
-};
+}
+
+//Ordenar de Menor a Mayor
+
+
+document.getElementById('example').addEventListener('change', sortByAscendant);
+function sortByAscendant() {
+  //console.log(event);
+  if (event.target[1]) {
+    alert("mayor a menor");
+    //console.log(orderDescendant(array))
+  }else {
+    alert('menor a mayor');
+    //console.log(orderAscendant(array))
+  }
+}
+
 
 document.getElementById("perception-cultural").addEventListener('click', linkPerceptionCultural);
 function linkPerceptionCultural() {
   display([sectionTwo, sectionFlags, sectionIndicator, sectionCountry], "none");
   display([sectionCategories]);
   const elementNode = document.getElementById('section-categories');
-  showAllIndicators(indicatorsPerceptionCult, elementNode)
+  window.dataLovers.showAllIndicators(indicatorsPerceptionCult, elementNode);
   getElementsforEvents('each-indicator', linkIndicatorsAllContries);
 }
 
@@ -175,7 +235,7 @@ function linkLiteracy() {
   display([sectionTwo, sectionFlags, sectionIndicator, sectionCountry], "none");
   display([sectionCategories]);
   const elementNode = document.getElementById('section-categories');
-  showAllIndicators(indicatorsLiteracy, elementNode);
+  window.dataLovers.showAllIndicators(indicatorsLiteracy, elementNode);
   getElementsforEvents('each-indicator', linkIndicatorsAllContries);
 }
 
@@ -184,9 +244,39 @@ function linkUnemployment() {
   display([sectionTwo, sectionFlags, sectionIndicator, sectionCountry], "none");
   display([sectionCategories]);
   const elementNode = document.getElementById('section-categories');
-  showAllIndicators(indicatorUnemployment, elementNode);
+  window.dataLovers.showAllIndicators(indicatorUnemployment, elementNode);
   getElementsforEvents('each-indicator', linkIndicatorsAllContries);
 }
+
+//// de data.js
+let indicatorGrales = (code) => {
+      const indiPer = WORLDBANK.PER.indicators.filter(indicator => indicator.indicatorCode == code);
+      const indiMex = WORLDBANK.MEX.indicators.filter(indicator => indicator.indicatorCode == code);
+      const indiBra = WORLDBANK.BRA.indicators.filter(indicator => indicator.indicatorCode == code);
+      const indiChl = WORLDBANK.CHL.indicators.filter(indicator => indicator.indicatorCode == code);
+      const indiGral = [indiBra,indiChl,indiMex,indiPer];
+       return indiGral;
+    };
+
+//retorna nombre paises
+const indicatorTableCountry = (code) => {
+  const pruebas21=[];
+  indicatorGrales(code).forEach(function(e) {
+    pruebas21.push(e[0]);
+  });
+   let fullNames = pruebas21.map(inventor => inventor.countryName);
+   return fullNames;
+  };
+
+//retorna data
+const indicatorTableData = (code) => {
+  const pruebas12=[];
+  indicatorGrales(code).forEach(function(e) {
+    pruebas12.push(e[0]);
+  });
+   let fullData = pruebas12.map(inventor => inventor.data);
+   return fullData;
+  };
 
 //
 function linkIndicatorsAllContries() {
@@ -197,9 +287,9 @@ function linkIndicatorsAllContries() {
   const codeIndicator = event.srcElement.dataset.code;
   // filterIndicatorCategory(indicatorsLiteracy, 'SE.TER.CUAT.MS.FE.ZS');
   const contries = indicatorTableCountry(codeIndicator);
-  start(contries)
-  const data = indicatorTableData(codeIndicator)
-  console.log(data);
+  start(contries);
+  indicatorTableData(codeIndicator);
+  //console.log(data);
   // console.log(filterYearAllContries("1980"));
 }
 
@@ -209,8 +299,8 @@ function start(codigo) {
 
 
                // creates <table> and <tbody> elements
-               table     = document.createElement("table");
-               tablebody = document.createElement("tbody");
+               const table = document.createElement("table");
+               const tablebody = document.createElement("tbody");
 
               const header = document.createElement("tr");
 
@@ -231,18 +321,19 @@ function start(codigo) {
                    //year = 5;
                   var percent = 2;
                  // creates a <tr> element
-                   mycurrent_row = document.createElement("tr");
+                  const mycurrent_row = document.createElement("tr");
 
 
                        // creates a <td> element's
-                       year_cell = document.createElement("td");
-                       percent_cell = document.createElement("td");
+                       const year_cell = document.createElement("td");
+                       const percent_cell = document.createElement("td");
                        // creates a Text Node
+                       let currenttext2 = "";
                  if (percent == "") { currenttext2 = document.createTextNode("no info.");
                    } else {
                      currenttext2 = document.createTextNode(percent);
                    }
-                       currenttext = document.createTextNode(year);
+                       const currenttext = document.createTextNode(year);
                        // appends the Text Node we created into the cell <td>
                        year_cell.appendChild(currenttext);
                        percent_cell.appendChild(currenttext2);
@@ -256,8 +347,8 @@ function start(codigo) {
                }
 
 
-                       years = document.createTextNode("Anos");
-                       percents = document.createTextNode("Porcentaje %");
+                      const years = document.createTextNode("Anos");
+                      const percents = document.createTextNode("Porcentaje %");
 
                        cellYears.appendChild(years);
                        cellPercent.appendChild(percents);
@@ -279,3 +370,35 @@ function getElementsforEvents(className, functionName) {
         element.addEventListener('click', functionName);
       });
 }
+
+
+
+ // //// de data.js
+ // let indicatorGrales = (code) => {
+ //       indiPer = WORLDBANK.PER.indicators.filter(indicator => indicator.indicatorCode == code);
+ //       indiMex = WORLDBANK.MEX.indicators.filter(indicator => indicator.indicatorCode == code);
+ //        indiBra = WORLDBANK.BRA.indicators.filter(indicator => indicator.indicatorCode == code);
+ //        indiChl = WORLDBANK.CHL.indicators.filter(indicator => indicator.indicatorCode == code);
+ //        indiGral = [indiBra,indiChl,indiMex,indiPer];
+ //        return indiGral;
+ //     };
+ //
+ // //retorna nombre paises
+ // indicatorTableCountry = (code) => {
+ //   const pruebas21=[];
+ //   indicatorGrales(code).forEach(function(e) {
+ //     pruebas21.push(e[0]);
+ //   });
+ //    fullNames = pruebas21.map(inventor => inventor.countryName);
+ //    return fullNames;
+ //   };
+ //
+ // //retorna data
+ // indicatorTableData = (code) => {
+ //   const pruebas12=[];
+ //   indicatorGrales(code).forEach(function(e) {
+ //     pruebas12.push(e[0]);
+ //   });
+ //    fullData = pruebas12.map(inventor => inventor.data);
+ //    return fullData;
+ //   };
